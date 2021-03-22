e1ec13773fe0f8018d82e91694e4f0d528f5684b
LucasMarodin28 <80289698+LucasMarodin28@users.noreply.github.com>
2021-03-22 18:59:59 -0400

    Add files via upload

---
+++ Marodin/JS/index.js
@@ -0,0 +1,13 @@
+$(document).ready(
+    function () {
+        alert("O documento carregou !")
+    }
+        
+)
+
+$("#bt_Inicio").click(
+function(){
+    alert('Opa, você clicou no Botão inicio');
+}
+
+)
