5a48cc00a68ec4ecae598c58f915aa4952c9ddf0
LucasMarodin28 <80289698+LucasMarodin28@users.noreply.github.com>
2021-05-03 21:44:11 -0400

    Add files via upload

---
+++ Js/index.js
@@ -0,0 +1,25 @@
+$(document).ready(
+    function () {
+        alert("Carregou!")
+    }
+)
+$.ajax({
+    type:'GET',
+    url:'http://192.168.10.85:9000/users',
+    success:function(data){
+$.each(data, function(i, user){
+    $('#tabela_user').append(
+        "<tr>"+
+            "<td>"+i+"</td>"+
+            "<td>"+user.guuid+"</td>"+
+            "<td>"+user.name+"</td>"+
+            "<td>"+user.cpf+"</td>"+
+            "<td>"+user.occupation+"</td>"+
+        </tr>"
+    )
+})
+    },
+error: function(data){
+    console.log(data);
+}
+})
